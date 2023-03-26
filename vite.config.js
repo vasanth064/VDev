c22a44b899dcac0df109e088568bc7e24cdf3262
vasanth064 <vasanthvdev@gmail.com>
2023-03-27 00:09:49 +0530

    Removed swc styled components plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import '@swc/plugin-styled-components';
 
 export default defineConfig({
   envPrefix: 'REACT_APP_',
-  plugins: [react({ plugins: [['@swc/plugin-styled-components', {}]] })],
+  plugins: [react()],
 });
