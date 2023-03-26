d364af3e7cacd9b7b4293c19bb8838f941561161
vasanth064 <vasanthvdev@gmail.com>
2023-03-27 00:07:20 +0530

    Added Yarn.lock file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import '@swc/plugin-styled-components';
 
 export default defineConfig({
   envPrefix: 'REACT_APP_',
-  plugins: [react()],
+  plugins: [react({ plugins: [['@swc/plugin-styled-components', {}]] })],
 });
